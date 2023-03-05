5b4e6b9c55d387d7f0eca3c5ffa153259390bc79
kabornblack <shaibu.kabiru@email.com>
2023-03-05 23:14:02 +0200

    Used node https to get API data from openweathermap

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function(req, res){
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=985f071e32d53a6bb66aad5c354b15f3&units=metric";
+  https.get(url, function(response){
+    console.log(response);
+  });
   res.send("Server is up and running");
 });
 
