518aa52a43b68543d223dacfb3d39bb70168566e
kabornblack <shaibu.kabiru@email.com>
2023-03-08 00:09:50 +0200

    Got data from openweathermap and JSONparse it into a javaScript object and and dig through to get the actual piece of data needed

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ app.get("/", function(req, res){
 
   const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=985f071e32d53a6bb66aad5c354b15f3&units=metric";
   https.get(url, function(response){
-    console.log(response);
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      console.log(temp);
+      console.log(description);
+
+    });
   });
   res.send("Server is up and running");
 });
