296d46520d3e2ebae2c0611ec7e04e53e2aec27d
kabornblack <shaibu.kabiru@email.com>
2023-03-08 21:31:17 +0200

    Used body-parser to post the current weather and temprature to the browser

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
+  res.sendFile(__dirname + ("/index.html"))
+});
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=985f071e32d53a6bb66aad5c354b15f3&units=metric";
+app.post("/", function(req, res){
+  console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const appkey = "985f071e32d53a6bb66aad5c354b15f3";
+  const unit = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=" +  appkey + "&units=" + unit +"";
   https.get(url, function(response){
     console.log(response.statusCode);
 
@@ -13,14 +22,15 @@ app.get("/", function(req, res){
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const description = weatherData.weather[0].description;
-      console.log(temp);
-      console.log(description);
-
+      const icon = weatherData.weather[0].icon;
+      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+      res.write("<p>The weather is currently " + description + "<p>");
+      res.write("<h1>The temprature in "+ query +" is " + temp + " degree celcius</h1>");
+      res.write("<img src=" + imageURL +">");
+      res.send();
     });
   });
-  res.send("Server is up and running");
-});
-
+})
 
 
 
