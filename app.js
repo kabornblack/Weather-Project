0eb0f7e16bcc8df0f0ef18f90c50dccf8e1bdbbd
kabornblack <shaibu.kabiru@email.com>
2023-08-14 16:27:53 +0300

    Final

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const fs = require("fs");
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
@@ -5,35 +7,54 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+const apiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
+console.log(process.env.OPEN_WEATHER_MAP_API_KEY);
+
+
 app.get("/", function(req, res){
   res.sendFile(__dirname + ("/index.html"))
 });
 
 app.post("/", function(req, res){
-  console.log(req.body.cityName);
   const query = req.body.cityName;
-  const appkey = "985f071e32d53a6bb66aad5c354b15f3";
+  // const appkey = "111f59133c12341f75bf2b761d6ed57c";
+  apiKey
   const unit = "metric";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=" +  appkey + "&units=" + unit +"";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid=" +  apiKey + "&units=" + unit +"";
   https.get(url, function(response){
     console.log(response.statusCode);
 
+    // response.on("data", function(data){
+    //   const weatherData = JSON.parse(data);
+    //   const temp = weatherData.main.temp;
+    //   const description = weatherData.weather[0].description;
+    //   const icon = weatherData.weather[0].icon;
+    //   const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+    //   res.write("<p>The weather is currently " + description + "<p>");
+    //   res.write("<h1>The temprature in "+ query +" is " + temp + " degree celcius</h1>");
+    //   res.write("<img src=" + imageURL +">");
+    //   res.send();
+    // });
     response.on("data", function(data){
       const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
-      const description = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-      res.write("<p>The weather is currently " + description + "<p>");
-      res.write("<h1>The temprature in "+ query +" is " + temp + " degree celcius</h1>");
-      res.write("<img src=" + imageURL +">");
-      res.send();
+      if (weatherData && weatherData.main && weatherData.main.temp && weatherData.weather && weatherData.weather[0] && weatherData.weather[0].description && weatherData.weather[0].icon) {
+        const temp = weatherData.main.temp;
+        const description = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+        res.write("<p>The weather is currently " + description + "<p>");
+        res.write("<h1>The temperature in "+ query +" is " + temp + " degree Celsius</h1>");
+        res.write("<img src=" + imageURL +">");
+        res.send();
+      } else {
+        res.send("Error retrieving weather data");
+      }
     });
   });
-})
+});
 
 
 
-app.listen(3000, function(){
-  console.log("Server is running at port 3000.")
+app.listen(8000, function(){
+  console.log("Server is running at port 8000.")
 });
